03a69576bbab1d90c1bd134b1d05063b98f9da00
Allen-0207 <box5519@gmail.com>
2026-02-15 21:58:51 +0800

    Implement q_insert_tail
    Add q_insert_tail() to insert an element at the tail of the queue, and
    allocating memory for string.
    
    Change-Id: I79f64901928fcf3489041a517f683ddad7ab84d1

--- queue.c
+++ queue.c
@@ -61,6 +61,25 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+
+    element_t *new_e = (element_t *) calloc(1, sizeof(element_t));
+    if (!new_e) {
+        return false;
+    }
+
+    INIT_LIST_HEAD(&new_e->list);
+
+    int length = strlen(s) + 1;
+    new_e->value = (char *) calloc(1, length);
+    if (!new_e->value) {
+        free(new_e);
+        return false;
+    }
+    snprintf(new_e->value, length, "%s", s);
+    list_add_tail(&new_e->list, head);
     return true;
 }
 
