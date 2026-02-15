51b71176decba60e404ca5e16dc4b0c56a82e346
Allen-0207 <box5519@gmail.com>
2026-02-15 22:01:10 +0800

    Implement q_remove_head
    Add q_remove_head() to remove an element from the head of the queue.
    
    This function unlink the head node from the queue but does not free
    its memory.
    
    Change-Id: I51b0863921e7d55a6a0f373ad90d6370f7e59458

--- queue.c
+++ queue.c
@@ -86,7 +86,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *remove_e = list_first_entry(head, element_t, list);
+    list_del(&remove_e->list);
+    if (sp && remove_e->value) {
+        snprintf(sp, bufsize, "%s", remove_e->value);
+    }
+    return remove_e;
 }
 
 /* Remove an element from tail of queue */
